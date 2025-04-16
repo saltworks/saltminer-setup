a444970dc9926dea1d738c799f6083ae3d5e8538
bryant-sw <bryant@saltworks.io>
2025-04-16 10:32:18 -0500

    Initial commit of core Docker Compose solution.

---
+++ paths-cloud.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+smcfg="/etc/saltworks/saltminer"
+
+echo -e "\nCreating configuration files."
+
+sudo mkdir -p $smcfg/nginx
+sudo cp ./nginx.conf $smcfg/nginx
+
+
+sudo mkdir $smcfg/agent
+sudo mkdir $smcfg/api
+sudo mkdir -p $smcfg/jobmanager/report-templates
+sudo mkdir $smcfg/python
+sudo mkdir $smcfg/ui-api
+
+sudo cp -r ./config/saltminer/python/* $smcfg/python
+sudo cp -r ./config/saltminer/agent/SourceConfigs/ $smcfg/agent/
+sudo cp ./config/saltminer/api/appsettings.json $smcfg/api
+sudo cp ./config/saltminer/ui-api/appsettings.json $smcfg/ui-api
+sudo cp -r ./config/saltminer/jobmanager/* $smcfg/jobmanager/
+
+echo -e "\nComplete.\n"
