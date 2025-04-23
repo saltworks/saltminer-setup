1aaaf0f213925783431ea13ff6fff3fd55641031
bryant-sw <bryant@saltworks.io>
2025-04-23 13:32:32 -0500

    Incremental changes to automate Fleet usage.

--- .dockerfiles/fleet-config.sh
+++ .dockerfiles/fleet-config.sh
@@ -21,17 +21,29 @@ policy='{
   "is_protected": false
 }'
 
-echo "Starting Fleet SaltMiner configuration"
+echo -e "Starting Fleet SaltMiner configuration\n"
 KIBANA_BASE_URL=$(jq -r .ApiConfig.KibanaBaseUrl /appsettings.json)
 ES_USER=$(jq -r .ApiConfig.ElasticUsername /appsettings.json)
 ES_PASS=$(jq -r .ApiConfig.ElasticPassword /appsettings.json)
 
-curl --header "Content-Type: application/json" \
+echo -e "Registering agent policy\n"
+agent_policy_response=$(curl --header "Content-Type: application/json" \
   --request POST \
   --header "kbn-xsrf: true" \
   -u $ES_USER:$ES_PASS \
   --data "$policy" \
-  "$KIBANA_BASE_URL/api/fleet/agent_policies?sys_monitoring=true"
+  "$KIBANA_BASE_URL/api/fleet/agent_policies?sys_monitoring=true")
 
+policy_id=$(echo "$agent_policy_response" | jq -r '.item.id')
+echo "Policy ID: $policy_id"
+
+echo -e "Enrollment key acquisition\n"
+key_response=$(curl \
+ --request GET "$KIBANA_BASE_URL/api/fleet/enrollment_api_keys" \
+ -u $ES_USER:$ES_PASS)
+
+TOKEN=$(echo $key_response | jq -r --arg policy_id $policy_id '.list[] | select(.active==true and .policy_id == $policy_id) | .api_key')
+
+echo "ENROLLMENT_TOKEN=$TOKEN" > /fleet-enroll-token
 
 echo "Fleet configuration complete."
